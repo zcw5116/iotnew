a9bee5b2568dd65e5b09c69cd8e3c91eda36a464
zhoucw <cqfrog@163.com>
2018-05-12 00:52:19 +0800

    NbCdrM5Analysis

---
+++ src/main/scala/com/zyuc/stat/iotNBLiuzk/analysis/realtime/NbCdrM5Analysis.scala
@@ -0,0 +1,124 @@
+package com.zyuc.stat.iotNBLiuzk.analysis.realtime
+
+import com.zyuc.iot.utils.DbUtils
+import org.apache.spark.sql.SaveMode
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.sql.functions._
+
+/**
+  * Created by zhoucw on 18-5-11 下午10:29.
+  */
+object NbCdrM5Analysis {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setMaster("local[2]").setAppName("NbM5Analysis_201805101400")
+    val sc = new SparkContext(sparkConf)
+    val sqlContext = new HiveContext(sc)
+
+    val appName = sc.getConf.get("spark.app.name")
+    val inputPath = sc.getConf.get("spark.app.inputPath", "/user/epciot/data/cdr/transform/nb/data")
+    val outputPath = sc.getConf.get("spark.app.outputPath", "/user/epciot/data/cdr/analy_realtime/nb")
+    val userPath = sc.getConf.get("spark.app.userPath", "/user/epciot/data/baseuser/data/")
+    val userDataTime = sc.getConf.get("spark.app.userDataTime", "20180510")
+
+    val dataTime = appName.substring(appName.lastIndexOf("_") + 1)
+    val d= dataTime.substring(2,8)
+    val h= dataTime.substring(8, 10)
+    val m5 = dataTime.substring(10, 12)
+
+    val nbpath = inputPath + "/d=" + d + "/h=" + h + "/m5=" + m5
+
+    val nbM5DF = sqlContext.read.format("orc").load(nbpath)
+    val nbM5Table = "spark_nbm5"
+    nbM5DF.registerTempTable(nbM5Table)
+
+    // 过滤NB的用户，将用户的数据广播出去，如果用户的数据过大， 需要修改参数spark.sql.autoBroadcastJoinThreshold，
+    // 具体根据spark web ui的DGA执行计划调整, spark.sql.autoBroadcastJoinThreshold值默认为10M
+    val userDataPath = userPath + "/d=" + userDataTime
+    val userDF = sqlContext.read.format("orc").load(userDataPath).filter("isnb='1'")
+    val tmpUserTable = "spark_tmpuser"
+    userDF.registerTempTable(tmpUserTable)
+    val userTable = "spark_user"
+    sqlContext.sql(
+      s"""
+         |cache table ${userTable}
+         |as
+         |select mdn, custid
+         |from ${tmpUserTable}
+         |where isnb = '1'
+       """.stripMargin)
+
+    // 关联custid
+    val nbTable = "spark_nb"
+    sqlContext.sql(
+      s"""
+         |select u.custid, n.prov, n.city,
+         |       n.l_datavolumefbcuplink as upflow,
+         |       n.l_datavolumefbcdownlink as downflow
+         |from ${nbM5Table} n, ${userTable} u
+         |where n.mdn = u.mdn
+       """.stripMargin).registerTempTable(nbTable)
+
+    // 统计分析
+     val resultStatDF = sqlContext.sql(
+       s"""
+          |select custid, prov, city, sum(upflow) as upflow, sum(downflow) as downflow
+          |from ${nbTable}
+          |group by custid, prov, city
+        """.stripMargin)
+
+    val resultDF = resultStatDF.
+      withColumn("gather_cycle", lit(dataTime + "00")).
+      withColumn("gather_date", lit(dataTime.substring(0,8))).
+      withColumn("gather_time", lit(dataTime.substring(8,12) + "00"))
+
+    val inflowDF = resultDF.selectExpr("gather_cycle","gather_date", "gather_time", "custid", "prov", "city", "upflow as gather_value").
+      withColumn("gather_type", lit("OUTFLOW"))
+
+    val outflowDF = resultDF.selectExpr("gather_cycle","gather_date", "gather_time", "custid", "prov", "city", "downflow as gather_value").
+      withColumn("gather_type", lit("INFLOW"))
+
+    // 将结果写入到hdfs
+    val outputResult = outputPath + "/d=" + d + "/h=" + h + "/m5=" + m5
+    inflowDF.unionAll(outflowDF).write.mode(SaveMode.Overwrite).format("orc").save(outputResult)
+
+    // 将结果写入到tidb
+    var dbConn = DbUtils.getDBConnection
+    dbConn.setAutoCommit(false)
+    val sql = s"insert into iot_ana_5min_nb_cdr(gather_cycle, gather_date, gather_time, cust_id, city, province, gather_type, gather_value) VALUES (?,?,?,?,?,?,?,?)"
+    val pstmt = dbConn.prepareStatement(sql)
+    val result = sqlContext.read.format("orc").load(outputResult).
+      map(x=>(x.getString(0), x.getString(1), x.getString(2), x.getString(3),
+        x.getString(4), x.getString(5), x.getDouble(6), x.getString(7))).collect()
+
+    var i = 0
+    for(r<-result){
+      val gather_cycle = r._1
+      val gather_date = r._2
+      val gather_time = r._3
+      val custid = r._4
+      val prov = r._5
+      val city = r._6
+      val gather_type = r._8
+      val gather_value = r._7
+
+      pstmt.setString(1, gather_cycle)
+      pstmt.setString(2, gather_date)
+      pstmt.setString(3, gather_time)
+      pstmt.setString(4, custid)
+      pstmt.setString(5, prov)
+      pstmt.setString(6, city)
+      pstmt.setString(7, gather_type)
+      pstmt.setDouble(8, gather_value)
+      pstmt.addBatch()
+      if (i % 1000 == 0) {
+        pstmt.executeBatch
+      }
+    }
+    pstmt.executeBatch
+    dbConn.commit()
+    pstmt.close()
+    dbConn.close()
+
+  }
+}

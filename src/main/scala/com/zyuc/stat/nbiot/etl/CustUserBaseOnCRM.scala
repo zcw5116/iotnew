2875fcdf687bc463410411ba587d7cfb927cae6d
hadoop <1090136337@qq.com>
2018-11-16 14:54:25 +0800

    iotNBLiuzk

--- src/main/scala/com/zyuc/stat/nbiot/etl/CustUserBaseOnCRM.scala
+++ src/main/scala/com/zyuc/stat/nbiot/etl/CustUserBaseOnCRM.scala
@@ -1,28 +1,67 @@
 package com.zyuc.stat.nbiot.etl
 
-import org.apache.spark.sql.SaveMode
+import com.zyuc.stat.utils.FileUtils
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.sql.{Row, SaveMode}
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Created by liuzk on 18-10-11 下午8:41.
+  *
+  * 每天将三天前的 CRM  数据更新  orc
   */
 object CustUserBaseOnCRM {
   def main(args: Array[String]): Unit = {
     val sparkConf = new SparkConf()//.setAppName("test").setMaster("local[2]")
     val sc = new SparkContext(sparkConf)
     val sqlContext = new HiveContext(sc)
-
+    val appName = sc.getConf.get("spark.app.name")
     val datatime = sc.getConf.get("spark.app.datatime", "20180510")
     val outputPath = sc.getConf.get("spark.app.outputpath", "/user/iot/data/baseuser/data/")
-    val inputPath = sc.getConf.get("spark.app.inputPath", "/user/iot/data/CRM/data/20180916")
+    val inputPath = sc.getConf.get("spark.app.inputPath", "/user/iot/data/metadata/CRM/")
+    val fileSystem = FileSystem.get(sc.hadoopConfiguration)
 
-    val crmDF = sqlContext.read.format("orc").load(inputPath)
+    val CRMTime = appName.substring(appName.lastIndexOf("_") + 1)
+    val CRMpath = inputPath + s"/JiTuanWangYun-DuanDaoDuanBaoZhangXiTong_${CRMTime}.txt"
+    val path = new Path(CRMpath)
+    println("----------------")
+    println(path.toString)
+    if (FileUtils.getFilesByWildcard(fileSystem, path.toString).length > 0) {
+      import sqlContext.implicits._
+      val df = sc.textFile(CRMpath).map(x => x.split("\t", 34)).filter(_.length !=1)
+        .map(x => (x(12),x(1), x(3),x(5), x(6),x(9),x(33), x(20), x(19),x(18)))
+        .toDF("STAT","MDN","custid","belocity","beloprov","prodtype","isnb","is4g","is3g","is2g")
+
+      df.filter("STAT!='%拆机%'")
+        .selectExpr("concat('86',MDN) as mdn","custid","prodtype","beloprov","belocity","isnb","is4g","is3g","is2g")
+        .coalesce(20).write.format("orc").mode(SaveMode.Overwrite).save(outputPath + "tmpToDay")
+
+      // 删除目录下的文件
+      val baseUser = new Path(outputPath + "d=" + datatime + "/*.orc")
+      fileSystem.globStatus(baseUser).foreach(x => fileSystem.delete(x.getPath(), false))
+      // 移动临时目录到文件到正式的目录
+      val tmpPath = new Path(outputPath + "tmpToDay/*.orc")
+      fileSystem.globStatus(tmpPath).foreach(x => {
+        val tmpLocation = x.getPath().toString
+        var dataLocation = tmpLocation.replace(outputPath + "tmpToDay/", outputPath + "d=" + datatime + "/")
+
+        val tmpPath = new Path(tmpLocation)
+        val dataPath = new Path(dataLocation)
+
+        if (!fileSystem.exists(dataPath.getParent)) {
+          fileSystem.mkdirs(dataPath.getParent)
+        }
+        fileSystem.rename(tmpPath, dataPath)
+      })
+    }
+
+    /*val crmDF = sqlContext.read.format("orc").load(inputPath)
     crmDF.filter("STAT!='%拆机%'")
       .selectExpr("concat('86',MDN) as mdn","CUST_ID as custid","PROD_TYPE as prodtype",
         "BELO_PROV_COMP as beloprov", "BELO_CITY_COMP as belocity",
         "IF_NB as isnb","IF_4G as is4g","IF_3G as is3g","IF_2G as is2g")
-        .repartition(10).write.format("orc").mode(SaveMode.Overwrite).save(outputPath + "d=" + datatime)
+        .repartition(10).write.format("orc").mode(SaveMode.Overwrite).save(outputPath + "d=" + datatime)*/
 
 
   }

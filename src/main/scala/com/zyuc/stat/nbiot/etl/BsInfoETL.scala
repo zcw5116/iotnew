b09aa57eee44db5e5e4c24a2e9901beb2a411d92
hadoop <1090136337@qq.com>
2019-06-28 16:11:45 +0800

    iotNBLiuzk

--- src/main/scala/com/zyuc/stat/nbiot/etl/BsInfoETL.scala
+++ src/main/scala/com/zyuc/stat/nbiot/etl/BsInfoETL.scala
@@ -1,5 +1,7 @@
 package com.zyuc.stat.nbiot.etl
 
+import com.zyuc.stat.utils.FileUtils
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.hadoop.io.{LongWritable, Text}
 import org.apache.hadoop.mapred.TextInputFormat
 import org.apache.spark.sql.SaveMode
@@ -15,9 +17,17 @@ object BsInfoETL {
     val sc = new SparkContext(sparkConf)
     val sqlContext = new HiveContext(sc)
 
+    val appName = sc.getConf.get("spark.app.name")
+    val inputPath = sc.getConf.get("spark.app.inputPath", "/user/iot/data/basic/IotBSInfo/src/")
+    val outputPath = sc.getConf.get("spark.app.outputPath","/user/iot/data/basic/IotBSInfo/data/")
+    val fileSystem = FileSystem.get(sc.hadoopConfiguration)
+
+    val dataTime = appName.substring(appName.lastIndexOf("_") + 1)
+
+
     import sqlContext.implicits._
-    val terminalTable = "bsTable"
-    val filePath = "/user/iot/tmp/bsinfo.csv"
+    val bsPath = inputPath + s"bts_info_$dataTime.csv"
+    val path = new Path(bsPath)
 
     /*val rddGBK2 = sc.hadoopFile(filePath, classOf[TextInputFormat], classOf[LongWritable], classOf[Text], 1).
       map(p => new String(p._2.getBytes, 0, p._2.getLength, "GBK"))
@@ -25,17 +35,15 @@ object BsInfoETL {
       .map(line=>line.replace("\"", "").split(",",9)).map(x=>(x(0),x(1),x(2),x(3),x(4),x(5),x(6),x(7),x(8)))
       .toDF("enbid", "provId", "provName", "cityId", "cityName", "zhLabel", "userLabel", "vendorId", "vndorName")
       .registerTempTable(terminalTable)*/
+    if (FileUtils.getFilesByWildcard(fileSystem, path.toString).length > 0) {
+      val df = sc.textFile(bsPath)
+        .filter(_.length>3)
+        .map(line=>line.replace("\"", "").split(",",9)).map(x=>(x(0),x(1),x(2),x(3),x(4),x(5),x(6),x(7),x(8)))
+        .toDF("enbid", "provId", "provName", "cityId", "cityName", "zhLabel", "userLabel", "vendorId", "vndorName")
+        .write.format("orc").mode(SaveMode.Overwrite)save(outputPath)
+    }
 
-    val rddGBK2 = sc.textFile(filePath)
-      .filter(_.length>3)
-      .map(line=>line.replace("\"", "").split(",",9)).map(x=>(x(0),x(1),x(2),x(3),x(4),x(5),x(6),x(7),x(8)))
-      .toDF("enbid", "provId", "provName", "cityId", "cityName", "zhLabel", "userLabel", "vendorId", "vndorName")
-      .registerTempTable(terminalTable)
 
-      val sql =
-        s"""
-           |select * from ${terminalTable}
-         """.stripMargin
-    sqlContext.sql(sql).write.format("orc").mode(SaveMode.Overwrite)save("/user/iot/data/basic/IotBSInfo/data/")
+
   }
 }
